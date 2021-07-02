fddffa09264d5035c06cbe59b0650b50b434d67b
garyfoo88 <reid9000200020002000@gmail.com>
2021-07-03 00:56:25 +0800

    save img to gallery

--- components/main/Save.js
+++ components/main/Save.js
@@ -1,8 +1,19 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Image,Button } from "react-native";
+import { StyleSheet, Text, View, TextInput, Image, Button } from "react-native";
+import firebase from "firebase";
 
 const Save = (props) => {
   const [caption, setCaption] = useState("");
+  const childPath = `post/${
+    firebase.auth().currentUser.uid
+  }/${Math.random().toString(36)}`;
+  const uploadImage = async () => {
+    const uri = props.route.params.image;
+    const response = await fetch(uri);
+    const blob = await response.blob();
+
+    const task = firebase.storage().ref().child(childPath).put(blob);
+  };
 
   return (
     <View style={{ flex: 1 }}>
@@ -13,6 +24,7 @@ const Save = (props) => {
           setCaption(e);
         }}
       />
+      <Button title="Save" onPress={() => {}} />
     </View>
   );
 };
